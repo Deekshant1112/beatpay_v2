a730dad43b5296441760546cbc5ffefc5d7e8edc
deekshant <deepurohit99@gmail.com>
2026-02-26 18:24:30 +0530

    Updated API for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,12 @@
 // services/api.js
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api', headers: { 'Content-Type': 'application/json' } });
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: `${BASE_URL}/api`,
+  headers: { 'Content-Type': 'application/json' }
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('bp_token');
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://beatpay-backend.onrender.com/',
         changeOrigin: true,
       },
       '/socket.io': {
